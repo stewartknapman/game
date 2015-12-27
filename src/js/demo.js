6c4e4e82b82c26742a17c33c55bb538dc9fda1ef
Stewart Knapman <info@stewartknapman.com>
2015-12-27 17:56:58 +1300

    basic game loop and state manager

---
+++ src/js/demo.js
@@ -0,0 +1,14 @@
+var Game = require('../../../src/game.js');
+var game = new Game('#main');
+
+var tick = 0;
+
+game.newState('demo', {
+  init: function () { console.log('init'); },
+  update: function () { console.log('update', tick); tick++; },
+  destroy: function () { console.log('destroy'); },
+  resize: function () { console.log('resize'); }
+});
+
+game.loadState('demo');
+// game.start();
